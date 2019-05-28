3b961d50ee92454e1944974decdc16a04ebd6e9a
Gloria Chen <cmxgloria@gmail.com>
2019-05-28 12:56:14 +1000

    function component

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      value: null,
-    }
-  }
-    render() {
-      return (
-        <button className="square" 
-        onClick={()=>this.props.onClick()}>
-          {this.props.value}
-        </button>
-      );
-    }
-  }
-  
+function square(props){
+  return (
+    <button className = "square" onClick={props.onClick}>
+    {props.value}
+    </button>
+  );
+}
   class Board extends React.Component {
     constructor(props){
       super(props);
       this.state={
         square: Array(9).fill(null),
+        xIsNext: true,
       };
     }
     handleClick(i){
