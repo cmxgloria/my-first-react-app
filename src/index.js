50469ed33f6821e449f8146c8b8d9389b9373027
Gloria Chen <cmxgloria@gmail.com>
2019-05-28 18:03:15 +1000

    what learned from proj

--- src/index.js
+++ src/index.js
@@ -144,3 +144,7 @@ function square(props){
     }
     return null;
   }
+  /*Lets you play tic-tac-toe,
+Indicates when a player has won the game,
+Stores a game’s history as a game progresses,
+Allows players to review a game’s history and see previous versions of a game’s board*/
