fe40bcb2af4ddcd7da5210822e5ffd9386c01209
Gloria Chen <cmxgloria@gmail.com>
2019-05-28 13:27:57 +1000

    declaring a winner

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ function square(props){
     }
     handleClick(i){
       const squares= this.state.squares.slice();
+      if( calculateWinner(square)||square[i]){
+       return;
+      }
       square[i]= this.state.xIsNext? 'X' : 'O';
       this.setState({squares:squares},
       xIsNext = !this.state.xIsNext,);
@@ -31,7 +34,13 @@ function square(props){
   
     render() {
       const status = 'Next player: '+ (this.state.xIsNext? 'X' : 'O');
-  
+  const winner = calculateWinner(this.state.squares);
+  let status;
+  if(winner){
+    status = 'Winner: '+ winner;
+}else{
+  status = "Next Player " + (this.state.xIsNext? 'X': 'O';)
+}
       return (
         <div>
           <div className="status">{status}</div>
@@ -70,7 +79,25 @@ function square(props){
       );
     }
   }
-  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   // ========================================
   
   ReactDOM.render(
